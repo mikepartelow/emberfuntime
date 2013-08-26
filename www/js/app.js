b51a4dba46248eae8e3206f16c90c588b298dee1
Mike Partelow <mikep@shoehater.com>
2013-08-26 15:19:32 -0700

    updates

--- www/js/app.js
+++ www/js/app.js
@@ -30,8 +30,16 @@ App.IssuesTableView = Ember.View.extend({
   
   didInsertElement: function() {
     $("#issues-table").tablesorter({
+      sortList: [ [3,0],[2,0] ],      
+      widthFixed: true,
+
+      headerTemplate : '{content} {icon}',
+      headers: {
+        0: { sorter: false }
+      },
+      
       theme:        'blue',
-      widgets:      [ 'zebra', ],
+      widgets:      [ 'zebra', 'filter', 'uitheme' ],
     });    
   }
 });
@@ -51,7 +59,15 @@ App.Issue = DS.Model.extend({
 	summary:      DS.attr('string'),
 	owner:        DS.attr('string'),
 	
-	description:  DS.attr('string')
+	priority:     DS.attr('number'),
+	status:       DS.attr('string'),
+	
+	description:  DS.attr('string'),	
+});
+
+Ember.Handlebars.helper('ymd', function(d, options) {	  
+  // convert a Date into YYYY/mm/dd because JS doesn't have strftime
+	return d.getFullYear().toString() + '/' + ('0' + (d.getMonth() + 1)).slice(-2) + '/' + ('0' + (d.getDate() + 1)).slice(-2);
 });
 
 App.Store = DS.Store.extend({
@@ -62,41 +78,51 @@ App.Store = DS.Store.extend({
 App.Issue.FIXTURES = [
   {
     id:             '123abc',
-    created_at:     '2013-02-07T16:44:57.000Z',
+    created_at:     new Date('2013-02-07'),
     summary:        'summary 1',
     owner:          'user 1',
+    priority:       1,
+    status:         'Open', 
     description:    "long\ndescription\none"   
   },
   
   {
     id:             'fa32123xyz',
-    created_at:     '2013-07-07T16:44:57.000Z',
+    created_at:     new Date('2013-07-07'),
     summary:        'summary 2',
     owner:          'user 2',
+    priority:       1,
+    status:         'Open', 
     description:    "long\ndescription\nsix"     
   },  
 
   {
     id:             '123x342432fsdyz',
-    created_at:     '2013-04-07T16:44:57.000Z',
+    created_at:     new Date('2013-04-07'),
     summary:        'summary 3',
     owner:          'user 2',
+    priority:       2,
+    status:         'Open',     
     description:    "long\ndescription\nseven"     
   },  
 
   {
     id:             '123xyzre231das',
-    created_at:     '2013-06-07T16:44:57.000Z',
+    created_at:     new Date('2013-06-06'),
     summary:        'summary 4',
     owner:          'user 2',
+    priority:       1,
+    status:         'Closed',     
     description:    "long\ndescription\neight"     
   },  
 
   {
     id:             '123xyzrew90232',
-    created_at:     '2013-01-07T16:44:57.000Z',
+    created_at:     new Date('2013-01-07'),
     summary:        'summary 5',
     owner:          'user 3',
+    priority:       3,
+    status:         'Open',     
     description:    "long\ndescription\nnine"     
   },  
   
